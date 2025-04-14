3c7a52a758bfeee4c0b95a294d6076c85c7f6797
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-04-14 19:00:49 -0300

    exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Ramon Ortiz',
+    })
+
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'temp.html')
 
 def sobre(request):
     return HttpResponse('sobre')
