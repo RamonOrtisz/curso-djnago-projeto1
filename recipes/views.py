64329d93b2b15fadb4970cee46cbd376a91ef1c7
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-04-14 19:48:34 -0300

    remove contact/about

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Ramon Ortiz',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
